c6f10eb849f7f0c7e2bb1b85a45bbc7a38fad638
Donald Pinckney <donald_pinckney@icloud.com>
2021-03-25 22:06:37 -0400

    Hacky but simpler verdaccio init

--- Scripts/npm/init_registry.sh
+++ Scripts/npm/init_registry.sh
@@ -6,23 +6,26 @@ killall Verdaccio
 
 echo "$PATH"
 
-# Start verdaccio
-output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
-verdaccio --config Configs/npm/verdaccio_config.yaml &> $output &
-server_pid=$!
-echo "Server pid: $server_pid"
-echo "Output: $output"
-echo "Wait:"
-until grep -q -i 'http addres' $output
-do
-  if ! ps $server_pid > /dev/null
-  then
-    echo "The server died" >&2
-    exit 1
-  fi
-  echo -n "."
-  sleep 1
-done
-echo
-echo "Server is running!"
-rm $output
+verdaccio --config Configs/npm/verdaccio_config.yaml &
+sleep 3
+
+## Start verdaccio
+#output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
+#verdaccio --config Configs/npm/verdaccio_config.yaml &> $output &
+#server_pid=$!
+#echo "Server pid: $server_pid"
+#echo "Output: $output"
+#echo "Wait:"
+#until grep -q -i 'http addres' $output
+#do
+#  if ! ps $server_pid > /dev/null
+#  then
+#    echo "The server died" >&2
+#    exit 1
+#  fi
+#  echo -n "."
+#  sleep 1
+#done
+#echo
+#echo "Server is running!"
+#rm $output
