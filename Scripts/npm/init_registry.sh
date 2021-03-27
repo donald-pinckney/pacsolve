7db1678b34c75cabed13c16a0622a04736fdc4e9
Donald Pinckney <donald_pinckney@icloud.com>
2021-03-26 23:08:18 -0400

    Go back to old init script

--- Scripts/npm/init_registry.sh
+++ Scripts/npm/init_registry.sh
@@ -6,26 +6,23 @@ killall Verdaccio
 
 echo "$PATH"
 
-verdaccio --config Configs/npm/verdaccio_config.yaml &
-sleep 3
-
-## Start verdaccio
-#output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
-#verdaccio --config Configs/npm/verdaccio_config.yaml &> $output &
-#server_pid=$!
-#echo "Server pid: $server_pid"
-#echo "Output: $output"
-#echo "Wait:"
-#until grep -q -i 'http addres' $output
-#do
-#  if ! ps $server_pid > /dev/null
-#  then
-#    echo "The server died" >&2
-#    exit 1
-#  fi
-#  echo -n "."
-#  sleep 1
-#done
-#echo
-#echo "Server is running!"
-#rm $output
+# Start verdaccio
+output=$(mktemp "${TMPDIR:-/tmp/}$(basename 0).XXX")
+verdaccio --config Configs/npm/verdaccio_config.yaml &> $output &
+server_pid=$!
+echo "Server pid: $server_pid"
+echo "Output: $output"
+echo "Wait:"
+until grep -q -i 'http addres' $output
+do
+  if ! ps $server_pid > /dev/null
+  then
+    echo "The server died" >&2
+    exit 1
+  fi
+  echo -n "."
+  sleep 1
+done
+echo
+echo "Server is running!"
+rm $output
