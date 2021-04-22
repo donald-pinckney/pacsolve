df4b353f61e27e2e3651b966036ef89f03b33a47
Donald Pinckney <donald_pinckney@icloud.com>
2021-04-21 23:31:25 -0400

    Write op defs, implement op interpreter loop

---
+++ Sources/DependencyRunner/DSL/DslRunner.swift
@@ -0,0 +1,34 @@
+
+struct EcosystemStore {
+    let contextResults: [ContextVar : SolveResult?]
+}
+
+func dictionaryOfNils<K, V>(forKeys: [K]) -> [K : V?] {
+    Dictionary(uniqueKeysWithValues: forKeys.map { ($0, nil) })
+}
+
+extension EcosystemProgram {
+    func run(underPackageManager p: PackageManager) -> EcosystemStore {
+        p.startup()
+
+        var contextResults: [ContextVar : SolveResult?] = Dictionary(uniqueKeysWithValues: self.declaredContexts.map { ($0, nil) })
+        
+        let contexts: [ContextVar : SolveContext] = Dictionary(uniqueKeysWithValues: self.declaredContexts.map { ($0, p.makeSolveContext()) })
+        
+        for op in self.ops {
+            switch op {
+            case let .publish(package: package, version: v, dependencies: deps):
+                p.publish(package: package, version: v, dependencies: deps)
+            case let .yank(package: package, version: v):
+                p.yank(package: package, version: v)
+            case let .solve(inContext: ctxVar, constraints: constraints):
+                let result = contexts[ctxVar]!(constraints)
+                contextResults[ctxVar] = result
+            }
+        }
+        
+        p.shutdown()
+        
+        return EcosystemStore(contextResults: contextResults)
+    }
+}
--- Sources/DependencyRunner/DSL/dsl.swift
+++ Sources/DependencyRunner/DSL/dsl.swift
@@ -8,58 +8,21 @@ struct DependencyExpr {
     let constraint: ConstraintExpr
 }
 
-struct EcosystemNode {
-    let package: Package
-    let version: Version
-    let dependencies: [DependencyExpr]
+//struct EcosystemNode {
+//    let package: Package
+//    let version: Version
+//    let dependencies: [DependencyExpr]
+//}
+
+typealias ContextVar = String
+
+enum EcosystemOp {
+    case publish(package: Package, version: Version, dependencies: [DependencyExpr])
+    case yank(package: Package, version: Version)
+    case solve(inContext: ContextVar, constraints: [DependencyExpr])
 }
 
-//struct Ecosystem {
-//    let nodes: [EcosystemNode]
-//    init(_ ecosystemDict: [Package : [Version : [DependencyExpr]]]) {
-//        var tmpNodes: [EcosystemNode] = []
-//        
-//        for (pkg, pkgDict) in ecosystemDict {
-//            for (v, deps) in pkgDict.sorted(by: { $0.key < $1.key }) {
-//                tmpNodes.append(EcosystemNode(package: pkg, version: v, dependencies: deps))
-//            }
-//        }
-//        
-//        nodes = tmpNodes
-//    }
-//}
-//
-//extension Ecosystem: Sequence {
-//    typealias Iterator = AnyIterator<EcosystemNode>
-//    
-//    func makeIterator() -> Iterator {
-//        var it = nodes.makeIterator()
-//        return AnyIterator {
-//            return it.next()
-//        }
-//    }
-//}
-//
-//extension Ecosystem: Collection {
-//    typealias Index = Int
-//    var startIndex: Int {
-//        nodes.startIndex
-//    }
-//    var endIndex: Int {
-//        nodes.endIndex
-//    }
-//    subscript(position: Int) -> Iterator.Element {
-//        precondition(indices.contains(position), "out of bounds")
-//        return nodes[position]
-//    }
-//    func index(after i: Int) -> Int {
-//        nodes.index(after: i)
-//    }
-//}
-//
-//extension PackageManager {
-//    func solve(_ ecosystem: Ecosystem, forRootPackage root: Package, version: Version) -> SolveResult {
-//        self.generate(ecosystem: ecosystem)
-//        return solve(package: root, version: version)
-//    }
-//}
+struct EcosystemProgram {
+    let declaredContexts: [ContextVar]
+    let ops: [EcosystemOp]
+}
