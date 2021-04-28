25e8cdf42274ec00e4f562e33222ac58c607f8cd
Donald Pinckney <donald_pinckney@icloud.com>
2021-04-27 22:19:52 -0400

    Put in republish results

--- Sources/DependencyRunner/Programs/Republish.swift
+++ Sources/DependencyRunner/Programs/Republish.swift
@@ -4,3 +4,8 @@ let program_Republish = EcosystemProgram(declaredContexts: ["ctx"], ops: [
     .publish(package: "a", version: "0.0.1", dependencies: []),
 ])
 
+/*
+ ["npm-real", "yarn1-real", "yarn2-real", "cargo-real", "pip-real"]
+ ERROR on 2nd publish
+
+ */
