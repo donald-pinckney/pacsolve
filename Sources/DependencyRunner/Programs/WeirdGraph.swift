22d0b44bbe9dcd879872723b02f94a51fc81ed61
Donald Pinckney <donald_pinckney@icloud.com>
2021-07-24 15:48:21 -0400

    Weird graph for testing

--- Sources/DependencyRunner/Programs/WeirdGraph.swift
+++ Sources/DependencyRunner/Programs/WeirdGraph.swift
@@ -3,6 +3,7 @@ let program_WeirdGraph = EcosystemProgram(declaredContexts: ["ctx"], ops: [
     .publish(package: "B", version: "1.0.0", dependencies: [DependencyExpr(packageToDependOn: "C", constraint: .any)]),
     .publish(package: "C", version: "1.0.0", dependencies: [DependencyExpr(packageToDependOn: "A", constraint: .any), DependencyExpr(packageToDependOn: "D", constraint: .any)]),
     .publish(package: "D", version: "1.0.0", dependencies: [DependencyExpr(packageToDependOn: "B", constraint: .any)]),
+    .publish(package: "E", version: "1.0.0", dependencies: []),
     .solve(inContext: "ctx", constraints: [DependencyExpr(packageToDependOn: "A", constraint: .any), DependencyExpr(packageToDependOn: "B", constraint: .any)])
 ])
 
