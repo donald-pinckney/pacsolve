b33946fa4c3c79b35b057d5513930f63e67afe3e
Donald Pinckney <donald_pinckney@icloud.com>
2021-09-21 16:23:35 -0400

    Add downloading to job starter

--- Experiments/package-downloader/start_jobs.bash
+++ Experiments/package-downloader/start_jobs.bash
@@ -5,6 +5,8 @@ pip install --upgrade tqdm --user
 
 num_jobs='10'
 
+wget -O all_packages_raw.json https://replicate.npmjs.com/_all_docs
+
 for ((my_job_id=0;my_job_id<num_jobs;my_job_id++)); do
-  sbatch --export=num_jobs=$num_jobs,my_job_id=$my_job_id job.bash
+  sbatch --export=raw_json='all_packages_raw.json',num_jobs=$num_jobs,my_job_id=$my_job_id job.bash
 done
