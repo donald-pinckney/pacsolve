c9e4fcd67a339a99ea01a96474885ace6ac588e9
Donald Pinckney <donald_pinckney@icloud.com>
2021-09-22 10:11:05 -0400

    remove download

--- Experiments/package-downloader/start_discovery_jobs.bash
+++ Experiments/package-downloader/start_discovery_jobs.bash
@@ -5,7 +5,7 @@ pip install --upgrade tqdm --user
 
 num_jobs='48'
 
-wget -O all_packages_raw.json https://replicate.npmjs.com/_all_docs
+# wget -O all_packages_raw.json https://replicate.npmjs.com/_all_docs
 
 for ((my_job_id=0;my_job_id<num_jobs;my_job_id++)); do
   sbatch --export=ALL,raw_json='all_packages.json',num_jobs=$num_jobs,my_job_id=$my_job_id job.bash
