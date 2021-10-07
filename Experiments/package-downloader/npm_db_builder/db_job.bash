065ec267d067dd18f3044099f474fb5d2e2d3f24
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-07 11:09:07 -0400

    Add job for db builder

---
+++ Experiments/package-downloader/npm_db_builder/db_job.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=8:00:00
+#SBATCH --job-name=npm_db
+#SBATCH --partition=short
+#SBATCH --mem=10G
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --output=%j.output
+#SBATCH --error=%j.error
+
+rm npm_db.sqlite3 npm_db.sqlite3-journal ; cargo run --release
