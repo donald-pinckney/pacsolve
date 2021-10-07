7492e047a81c836cb159e726b8a95f58f32acbfd
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-07 14:50:31 -0400

    x

--- Experiments/package-downloader/npm_db_builder/db_job.bash
+++ Experiments/package-downloader/npm_db_builder/db_job.bash
@@ -6,8 +6,8 @@
 #SBATCH --mem=64G
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --output=log.output
-#SBATCH --error=log.error
+#SBATCH --output=%j.output
+#SBATCH --error=%j.error
 
 rm npm_db.sqlite3 npm_db.sqlite3-journal ; RUST_BACKTRACE=full cargo run --release
 
