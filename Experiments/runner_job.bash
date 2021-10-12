e795845c929209c02ff0f6fcf405b797aa60c2cd
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-12 12:40:58 -0400

    update job

---
+++ Experiments/runner_job.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name=npm-rosette-experiment
+#SBATCH --partition=short
+#SBATCH --mem=32G
+#SBATCH -N 1
+#SBATCH -n 1
+
+python3 Runner/main.py --dataset nontesting --only supports-color --cleanup --configs npm '~/.npm-packages/bin/npm install --production' rosette '~/.npm-packages/bin/npm install --rosette'
