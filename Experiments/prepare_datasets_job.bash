8d7fa7ba57ec74243c9d954711bf60d7eab48b6c
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-20 10:10:31 -0400

    more mem, because why not

--- Experiments/prepare_datasets_job.bash
+++ Experiments/prepare_datasets_job.bash
@@ -3,5 +3,6 @@
 #SBATCH --time=12:00:00
 #SBATCH --job-name=prepare_datasets_job
 #SBATCH --partition=short
+#SBATCH --mem=16G
 
 time python3 DatasetSetup/main.py --sqlite-path /work/arjunguha-research-group/packages/npm_db.sqlite3
