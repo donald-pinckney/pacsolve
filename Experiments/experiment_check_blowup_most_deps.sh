092a3ccae35cdb60b7786c2fb5f32c085f433e6c
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-20 14:59:00 -0400

    script for new experiment

--- Experiments/experiment_check_blowup_most_deps.sh
+++ Experiments/experiment_check_blowup_most_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 Runner/main.py \
-  --dataset nontesting_most_downloads \
-  --all \
+  --dataset nontesting_most_deps \
+  --range :50 \
   --configs \
   npm 'npm install --omit dev --omit optional --omit peer --ignore-scripts' \
   rosette 'npm install --rosette --ignore-scripts'
