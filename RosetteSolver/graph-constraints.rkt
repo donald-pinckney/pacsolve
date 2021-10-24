2632bc1e09b317025cee2dd4a78759895c6ad94e
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-24 18:17:39 -0400

    simplify solver some

--- RosetteSolver/graph-constraints.rkt
+++ RosetteSolver/graph-constraints.rkt
@@ -11,54 +11,47 @@
 ;;; -------------------------------------------
 
 
-;;; *** Constraints part 1: Checking well-formedness (edges have valid indices into node lists), and we have non-negative number of vertices
-(define (check-edge-well-formed e g)
-  (define p (edge-package-idx e)) ; not symbolic
-  (define v (edge-version-idx e)) ; symbolic, already properly bounded
-  (define n (edge-node-idx e)) ; symbolic, not propertly bounded by size of destination vector
 
-  (define p-group (list-ref (graph-package-groups-list g) p))
-  (define v-group (vector-ref (package-group-version-groups-vec p-group) v))
-  
-  (assert (<= 0 n))
-  (assert (< n (version-group-node-count v-group))))
-  
-(define (check-version-group-well-formed v-group _package)
-  (assert (<= 0 (version-group-node-count v-group))))
-
-(define (check-graph-well-formed query g)
-  (for/graph-edges query g (lambda (e _constraint _p _v _n) (check-edge-well-formed e g)))
-  (for/graph-version-groups g check-version-group-well-formed))
-
-
-;;; *** Constraints part 2: Checking that the graph is a DAG. This check can simply be omitted to allow cyclic graphs
+;;; *** Constraints part 1: Checking that the graph is a DAG. 
+;;; This check can simply be omitted to allow cyclic graphs
 (define (check-graph-acyclic query g)
   (assert (= 0 (node-top-order (graph-context-node g))))
   (for/graph-edges query g
     (lambda (e c _sp _sv src-node)
       (define dp-idx (edge-package-idx e)) ; not symbolic
       (define dv-idx (edge-version-idx e)) ; symbolic
-      (define dn-idx (edge-node-idx e)) ; symbolic
 
       (define dp-group (list-ref (graph-package-groups-list g) dp-idx))
-      (define dv-group (vector-ref (package-group-version-groups-vec dp-group) dv-idx))
-      (define dn (list-ref (version-group-nodes-list dv-group) dn-idx)) ; the destination node
+      (define dv-node (vector-ref (package-group-version-nodes-vec dp-group) dv-idx))
       
-      (assert (< (node-top-order src-node) (node-top-order dn))))))
+      (assert (< (node-top-order src-node) (node-top-order (version-node dv-node)))))))
                                              
   
 
-;;; *** Constraints part 3: Checking that the graph satisfies all dependency constraints
+;;; *** Constraints part 2: Checking that the graph satisfies all dependency constraints
 (define (sat/version-constraint v-s c)
   (c v-s))
 
 (define (check-graph-sat-deps query g)
-  (for/graph-edges query g (lambda (e constraint _p _v _n)
-    (define dest-version (car (registry-ref query (edge-package-idx e) (edge-version-idx e))))
-    (assert (sat/version-constraint dest-version constraint)))))
+  (assert (node-active (graph-context-node g)))
+  (for/graph-edges query g (lambda (e constraint _p _v src-node)
+    (if (node-active src-node)
+      (begin
+        (define dp-idx (edge-package-idx e)) ; not symbolic
+        (define dv-idx (edge-version-idx e)) ; symbolic
+
+        (define dp-group (list-ref (graph-package-groups-list g) dp-idx))
+        (define dv-node (vector-ref (package-group-version-nodes-vec dp-group) dv-idx))
+
+        (define dest-version (version-node-version dv-node))
+
+        (assert (node-active (version-node-node dv-node)))
+        (assert (sat/version-constraint dest-version constraint))
+      )
+      #t))))
 
 
-;;; *** Constraints part 4: Checking that the graph contains pairwise consistent versions of the same package, parameterized by relation `r`
+;;; *** Constraints part 3: Checking that the graph contains pairwise consistent versions of the same package, parameterized by relation `r`
 (define (check-graph-consistent query g r)
   (for-each
    (lambda (p-idx)
@@ -68,17 +61,17 @@
      (for-each
       (lambda (v1-idx)
         (define v1 (car (registry-ref query p-idx v1-idx)))
-        (define v1-group (vector-ref (package-group-version-groups-vec p-group) v1-idx)) ; the version group for v1
-        (define v1-count (version-group-node-count v1-group))
+        (define v1-version-node (vector-ref (package-group-version-nodes-vec p-group) v1-idx)) ; the version node for v1
+        (define v1-active (node-active (version-node-node v1-version-node)))
         
         (for-each
          (lambda (v2-idx)
            (define v2 (car (registry-ref query p-idx v2-idx)))
-           (define v2-group (vector-ref (package-group-version-groups-vec p-group) v2-idx)) ; the version group for v2
-           (define v2-count (version-group-node-count v2-group))
+           (define v2-version-node (vector-ref (package-group-version-nodes-vec p-group) v2-idx)) ; the version node for v2
+           (define v2-active (node-active (version-node-node v2-version-node)))
 
            (if (not (r v1 v2)) ; v1 and v2 are concrete
-               (assert (not (and (< 0 v1-count) (< 0 v2-count)))) ; v1-count and v2-count are symbolic
+               (assert (not (and v1-active v2-active))) ; v1-active and v2-active are symbolic
                #t))
          (range v1-idx)))
       (range n-vers)))
@@ -89,7 +82,7 @@
   (define (consistency-rel v1 v2) (evaluate-consistency query v1 v2))
   (define check-acyclic (options-check-acyclic (query-options query)))
 
-  (check-graph-well-formed query g)
   (check-graph-sat-deps query g)
   (if check-acyclic (check-graph-acyclic query g) #t)
-  (check-graph-consistent query g consistency-rel))
+  (check-graph-consistent query g consistency-rel)
+)
