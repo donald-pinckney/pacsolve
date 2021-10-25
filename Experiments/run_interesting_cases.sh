a00266ab43eeb4f00e9a8f6431e8dc6eeefa3a9f
Donald Pinckney <donald_pinckney@icloud.com>
2021-10-25 15:27:47 -0400

    interesting cases script

---
+++ Experiments/run_interesting_cases.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 Runner/main.py \
+  --dataset interesting_cases \
+  --all \
+  --configs \
+  npm 'npm install --omit dev --omit optional --omit peer --ignore-scripts' \
+  rosette 'npm install --rosette --ignore-scripts'
