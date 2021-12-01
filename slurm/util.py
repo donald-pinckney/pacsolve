80e46325b74033f2fee6062f67233f47d20c49a3
Arjun Guha <a.guha@northeastern.edu>
2021-11-30 21:15:29 -0500

    Faster experiment, back to ~30mins
    - [X] The Run class should receive an array of mode_configurations.
    - [X] The SOLVE_COMMAND should be calculated from a mode_configuration
          in a separate function.
    - [X] The Run class should handle preparing
          - [X] Constructor should receive a tarball source directory.
          - [X] Tweak list_pkg_paths: go through the tarball list and
                configuration list. The current check for experiment.json
                is sufficient for the case where the package is not prepared!
          - [X] Tweak run_minnpm: it needs to prepare the path if needed.

--- slurm/util.py
+++ slurm/util.py
@@ -1,3 +1,7 @@
+import json
+from typing import List, Any, Iterable
+from more_itertools import chunked, distribute
+
 def remove_nones(seq):
     return [x for x in seq if x is not None]
 
@@ -26,3 +30,15 @@ def write_json(path, data):
 def read_json(path: str) -> any:
     with open(path, 'r') as f_in:
         return json.load(f_in)
+
+def chunked_or_distributed(
+    items: Iterable[Any],
+    max_groups: int,
+    optimal_group_size: int) -> Iterable[Iterable[Any]]:
+    """Divide *items* into at most *max_groups*. If possible, produces fewer
+    than *max_groups*, but with at most *optimial_group_size* items in each
+    group."""
+    if len(items) / optimal_group_size <= max_groups:
+        return chunked(items, optimal_group_size)
+    else:
+        return distribute(items, max_groups)
