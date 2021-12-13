9bcd0556ccaf370ef5971f38ec2458ca33f1a0e3
Donald Pinckney <donald_pinckney@icloud.com>
2021-12-13 13:55:06 -0500

    a bit more timeout on case studies

--- Experiments/case_studies/significant_shrinkage/run.sh
+++ Experiments/case_studies/significant_shrinkage/run.sh
@@ -3,6 +3,6 @@
 here=$(dirname "$0")
 
 python3 Runner/main.py \
-  --dataset nontesting_most_downloads --only $(cat $here/projects.txt) --timeout 300 --configs \
+  --dataset nontesting_most_downloads --only $(cat $here/projects.txt) --timeout 800 --configs \
   npm 'npm install --omit dev --omit optional --omit peer --ignore-scripts' \
   minnpm_min_deps 'npm install --rosette --ignore-scripts --consistency npm --minimize min_num_deps,min_oldness'
