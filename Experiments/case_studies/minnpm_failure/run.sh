c3f689443903935fd4b82006758a71fefe7e3ef1
Donald Pinckney <donald_pinckney@icloud.com>
2021-12-13 13:48:51 -0500

    up the timeouts on case studies

--- Experiments/case_studies/minnpm_failure/run.sh
+++ Experiments/case_studies/minnpm_failure/run.sh
@@ -3,6 +3,6 @@
 here=$(dirname "$0")
 
 python3 Runner/main.py \
-  --dataset nontesting_most_downloads --only $(cat $here/projects.txt) --timeout 60 --configs \
+  --dataset nontesting_most_downloads --only $(cat $here/projects.txt) --timeout 300 --configs \
   npm 'npm install --omit dev --omit optional --omit peer --ignore-scripts' \
   minnpm_npm 'npm install --rosette --ignore-scripts --consistency npm --minimize min_oldness,min_num_deps'
