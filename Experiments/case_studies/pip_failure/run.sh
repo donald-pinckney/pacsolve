97df53c4439a4a7426e8933c0cd503eb25864d00
Donald Pinckney <donald_pinckney@icloud.com>
2021-12-13 13:57:57 -0500

    save logs

--- Experiments/case_studies/pip_failure/run.sh
+++ Experiments/case_studies/pip_failure/run.sh
@@ -7,4 +7,4 @@ python3 Runner/main.py \
   npm 'npm install --omit dev --omit optional --omit peer --ignore-scripts' \
   minnpm_pip 'npm install --rosette --ignore-scripts --consistency pip --minimize min_oldness,min_num_deps' \
   minnpm_npm 'npm install --rosette --ignore-scripts --consistency npm --minimize min_oldness,min_num_deps' \
-  minnpm_min_dups 'npm install --rosette --ignore-scripts --consistency npm --minimize min_duplicates'
+  minnpm_min_dups 'npm install --rosette --ignore-scripts --consistency npm --minimize min_duplicates' > $here/log.txt
--- Experiments/case_studies/vanilla_failure/run.sh
+++ Experiments/case_studies/vanilla_failure/run.sh
@@ -5,4 +5,4 @@ here=$(dirname "$0")
 python3 Runner/main.py \
   --dataset nontesting_most_downloads --only $(cat $here/projects.txt) --timeout 800 --configs \
   npm 'npm install --omit dev --omit optional --omit peer --ignore-scripts' \
-  minnpm_npm 'npm install --rosette --ignore-scripts --consistency npm --minimize min_oldness,min_num_deps'
+  minnpm_npm 'npm install --rosette --ignore-scripts --consistency npm --minimize min_oldness,min_num_deps' > $here/log.txt
