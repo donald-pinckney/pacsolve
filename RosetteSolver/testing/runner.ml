6547b90aa097ae6c213015ae6f46163caf1bdd5a
elleven11 <federico.cassano@federico.codes>
2022-03-26 01:13:16 -0400

    tested all "unexpecteds", made concurrent tests and pass/fail

--- RosetteSolver/testing/runner.ml
+++ RosetteSolver/testing/runner.ml
@@ -6,6 +6,8 @@ open Printf
 open OUnit2
 open ExtLib
 
+let () = OUnitThreads.init ()
+
 let result_printer (e : ('a, string) result) : string =
   match e with
   | Error v -> sprintf "Error: %s\n" v
@@ -41,38 +43,88 @@ let make_tmpfiles (name : string) (std_input : string) : tempfiles =
     stderr_name,
     stdin_read )
 
-let run (filename : string) : (graph, string) result =
-  let rstdout, rstdout_name, rstderr, rstderr_name, rstdin = make_tmpfiles "run" "" in
-  let ran_pid =
-    (* TODO : fix this whole relpath funny business*)
-    Unix.create_process "racket"
-      (Array.of_list
-         [ "racket";
-           "../../../rosette-solver.rkt";
-           "../../input/" ^ filename;
-           "../../actual/" ^ filename ] )
-      rstdin rstdout rstderr
-  in
-  let _, status = waitpid [] ran_pid in
+let run (filename : string) (timeout : int) : (graph, string) result =
   let result =
-    match status with
-    | WEXITED 0 -> (
-        let path = "../../actual/" ^ filename in
-        match parse_to_graph (string_of_file path) with
-        | Ok graph ->
-            make_dotgraph_file (path ^ ".dot") graph;
-            Ok graph
-        | Error msg -> Error msg )
-    | WEXITED n -> Error (sprintf "Exited with %d: %s" n (string_of_file rstderr_name))
-    | WSIGNALED n -> Error (sprintf "Signalled with %d while running %s." n filename)
-    | WSTOPPED n -> Error (sprintf "Stopped with signal %d while running %s." n filename)
+    try
+      let rstdout, rstdout_name, rstderr, rstderr_name, rstdin = make_tmpfiles "run" "" in
+      let ran_pid =
+        (* TODO : fix this whole relpath funny business*)
+        Unix.create_process "racket"
+          (Array.of_list
+             [ "racket";
+               "../../../rosette-solver.rkt";
+               "../../input/" ^ filename;
+               "../../actual/" ^ filename ] )
+          rstdin rstdout rstderr
+      in
+      (* creates a signal handler, that will kill the ran_pid if it gets the sigalrm signal. *)
+      let sighandler =
+        Sys.signal Sys.sigalrm (Sys.Signal_handle (fun _ -> Unix.kill ran_pid Sys.sigterm))
+      in
+      (* send sigalrm after timeout seconds *)
+      let _ = Unix.alarm timeout in
+      let result =
+        try
+          let _, status = waitpid [] ran_pid in
+          match status with
+          | WEXITED 0 -> (
+              let path = "../../actual/" ^ filename in
+              match parse_to_graph (string_of_file path) with
+              | Ok graph ->
+                  make_dotgraph_file (path ^ ".dot") graph;
+                  Ok graph
+              | Error msg -> Error msg )
+          | WEXITED n -> Error (sprintf "Exited with %d: %s" n (string_of_file rstderr_name))
+          | WSIGNALED n -> Error (sprintf "Signalled with %d while running %s." n filename)
+          | WSTOPPED n -> Error (sprintf "Stopped with signal %d while running %s." n filename)
+        with Unix.Unix_error (Unix.EINTR, _, _) ->
+          Sys.set_signal Sys.sigalrm sighandler;
+          Error (sprintf "Timedout (timeout = %d) while running %s" timeout filename)
+      in
+      List.iter close [rstdout; rstderr; rstdin];
+      List.iter unlink [rstdout_name; rstderr_name];
+      result
+    with Unix.Unix_error (err, _, _) ->
+      Error (sprintf "Unexpected error: %s while running %s" (Unix.error_message err) filename)
   in
-  List.iter close [rstdout; rstderr; rstdin];
-  List.iter unlink [rstdout_name; rstderr_name];
   result
 
-let test_run (name : string) (test_ctxt : OUnit2.test_ctxt) : unit =
+type pass_fail =
+  | TestPass
+  | TestFail
+
+let test_run_pass_fail
+    (name : string)
+    (timeout : int)
+    (pf : pass_fail)
+    (test_ctxt : OUnit2.test_ctxt) : unit =
+  let filename = name ^ ".json" in
+  let output = run filename timeout in
+  let result_pf, msg =
+    match output with
+    | Ok _ -> (TestPass, "")
+    | Error err -> (TestFail, err)
+  in
+  assert_equal pf result_pf ~printer:(fun e ->
+      match e with
+      | TestPass -> "Test passed"
+      | TestFail -> "Test failed: " ^ msg )
+
+let test_run (name : string) (timeout : int) (test_ctxt : OUnit2.test_ctxt) : unit =
   let filename = name ^ ".json" in
   let expected = parse_to_graph (string_of_file ("../../expected/" ^ filename)) in
-  let result = run filename in
+  let result = run filename timeout in
   assert_equal expected result ~printer:result_printer
+
+let test_run_error
+    (name : string)
+    (error_msg : string)
+    (timeout : int)
+    (test_ctxt : OUnit2.test_ctxt) : unit =
+  let filename = name ^ ".json" in
+  let result = run filename timeout in
+  assert_equal (Error error_msg) result ~printer:result_printer ~cmp:(fun check result ->
+      match (check, result) with
+      (* if a part of the expected string is contained inside the actual string *)
+      | Error e_msg, Error a_msg -> String.exists a_msg e_msg
+      | _ -> false )
--- RosetteSolver/testing/test.ml
+++ RosetteSolver/testing/test.ml
@@ -1,10 +1,41 @@
 open OUnit2
 open Runner
 
-let tfile (name : string) : OUnit2.test = name >:: test_run name
+let def_TIMEOUT = 30
+
+(* To run a test with an exact expected output *)
+let t ?(timeout = def_TIMEOUT) (name : string) : OUnit2.test = name >:: test_run name timeout
+
+(* To run a test with an exact expected outputted error*)
+let te ?(timeout = def_TIMEOUT) (name : string) (error_msg : string) : OUnit2.test =
+  name >:: test_run_error name error_msg timeout
+
+(* To run a test and expect the program to succeed *)
+let tp ?(timeout = def_TIMEOUT) (name : string) : OUnit2.test =
+  name >:: test_run_pass_fail name timeout TestPass
+
+(* To run a test and expect the program to fail *)
+let tf ?(timeout = def_TIMEOUT) (name : string) : OUnit2.test =
+  name >:: test_run_pass_fail name timeout TestFail
 
 let exact_output_suite : OUnit2.test =
   "exact_output_suite"
-  >::: [tfile "to-width"; tfile "string.prototype.split"]
+  >::: [ t "to-width";
+         t "string.prototype.split";
+         t "protobufjs" ~timeout:20;
+         te "istanbul-lib-instrument"
+           "Timedout (timeout = 30) while running istanbul-lib-instrument.json";
+         (* This one takes way too long *)
+         te "@jest_test-sequencer" "Timedout (timeout = 30) while running @jest_test-sequencer.json";
+         (* These two (eslints) were reported as "unexpected" but works here? *)
+         t "@eslint_eslintrc";
+         t "eslint";
+         t "@babel_plugin-transform-runtime" ~timeout:120;
+         t "jest-changed-files";
+         (* On this one racket crashes... *)
+         te "@istanbuljs_load-nyc-config" "read-solution: unrecognized solver output: #<eof>"
+           ~timeout:120;
+         te "node-libs-browser" "hash-ref: no value found for key\n  key: \"lodash.upperfirst\"";
+         te "crypto-browserify" "hash-ref: no value found for key\n  key: \"lodash.upperfirst\"" ]
 
 let () = run_test_tt_main exact_output_suite
