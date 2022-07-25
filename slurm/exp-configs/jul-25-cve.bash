2e8d7a2dc54aba53f9590248c62baea42072789c
Donald Pinckney <donald_pinckney@icloud.com>
2022-07-25 12:39:11 -0400

    x

--- slurm/exp-configs/jul-25-cve.bash
+++ slurm/exp-configs/jul-25-cve.bash
@@ -1,6 +1,7 @@
 export EXPERIMENT_DIR=/scratch/pinckney.d/jul-25-cve/
 export Z3_LOC=/work/arjunguha-research-group/pacsolve/z3/build/z3
 export Z3_MODEL_OPTION=True
+export TARBALL_DIR=/work/arjunguha-research-group/pacsolve/slurm/top1000tarballs
 
 mkdir -p $EXPERIMENT_DIR
-./main.py run --tarball-dir /work/arjunguha-research-group/pacsolve/slurm/top1000tarballs --z3-abs-path $Z3_LOC --z3-add-model-option $Z3_MODEL_OPTION --target $EXPERIMENT_DIR
+./main.py run --tarball-dir $TARBALL_DIR --z3-abs-path $Z3_LOC --z3-add-model-option $Z3_MODEL_OPTION --target $EXPERIMENT_DIR
