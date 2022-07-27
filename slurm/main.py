169755357570a3c124efa95be7d81d77866a61be
Donald Pinckney <donald_pinckney@icloud.com>
2022-07-27 18:31:22 -0400

    hack more on timeout bug and cve eval

--- slurm/main.py
+++ slurm/main.py
@@ -13,9 +13,10 @@ import csv
 import concurrent.futures
 import ast
 from tqdm import tqdm
-from typing import Any, Optional, Dict
+from typing import Any, Iterator, List, Optional, Dict, Tuple
 from contextlib import contextmanager
 import traceback
+import itertools
 
 from util import suppressed_iterator, write_json, read_json, chunked_or_distributed
 from random import shuffle
@@ -43,10 +44,34 @@ def gather(argv):
     Gather(args.directory).gather()
 
 
+DUMP_ERRORS = open('/dev/null', 'w')
+
+memoized_cve_badness = dict()
+
+def lookup_cve_badness(module_path: str, name: str, pack: Dict[str, Any], package_lock_path: str) -> float:
+    if 'version' not in pack:
+        print(f'Error (ignored): no version for {module_path} in {package_lock_path}', file=sys.stderr)
+        return 0
+
+    version = pack['version']
+    key = f'{name}:{version}'
+    if key not in memoized_cve_badness:
+        memoized_cve_badness[key] = float(subprocess.check_output([
+                '../version-cve-badness/version-cve-badness.sh', 
+                name, 
+                version],
+                stderr=DUMP_ERRORS
+            ).decode(
+                'utf-8', 
+                errors='ignore'
+            ).strip())
+    return memoized_cve_badness[key]
+
 class SolveResultEvaluation(object):
-    def __init__(self, time, status, lock_json: Optional[Dict[str, Any]]) -> None:
+    def __init__(self, time, status, lock_json: Optional[Dict[str, Any]], lock_path: str) -> None:
         self.time = time
         self.status = status
+        self.lock_path = lock_path
         if lock_json is None:
             self.num_deps = 0
             self.cve_badness = 0
@@ -61,16 +86,24 @@ class SolveResultEvaluation(object):
     def to_row_values(self):
         return [self.time, self.num_deps, self.status]
 
+    def all_packages(self, lock_json: Dict[str, Any]) -> List[Tuple[str, Dict[str, Any]]]:
+        return [(p, pack) for (p, pack) in lock_json['packages'].item()]
+    
+    def non_link_packages(self, lock_json: Dict[str, Any]) -> List[Tuple[str, Dict[str, Any]]]:
+        return [(p, pack) for (p, pack) in self.all_packages(lock_json) if not ('link' in pack and pack['link'])]
+
     def evaluate_num_deps(self, lock_json: Dict[str, Any]):
-        n = 0
-        for _, v in lock_json['packages'].items():
-            if 'link' in v and v['link']:
-                continue
-            n += 1
-        return n
+        return len(self.non_link_packages(lock_json))
+
+    def strip_node_modules_from_name(self, module_path: str) -> str:
+        parts = module_path.split('/')
+        parts.reverse()
+        parts = list(itertools.takewhile(lambda s: s != 'node_modules', parts))
+        parts.reverse()
+        return '/'.join(parts)
 
     def evaluate_cve_badness(self, lock_json: Dict[str, Any]):
-        return 0
+        return sum(lookup_cve_badness(module_path, self.strip_node_modules_from_name(module_path), pack, self.lock_path) for (module_path, pack) in self.non_link_packages(lock_json))
 
 
 class Gather(object):
@@ -92,31 +125,6 @@ class Gather(object):
         """
         p = os.path.join(self.directory, solver)
         return [f for f in os.listdir(p) if os.path.isdir(os.path.join(p, f))]
-
-    def num_deps(self, project):
-        """
-        Calculates the number of dependencies. This function assumes that
-        'npm install' was sucessful. It is critical that the check is performed
-        correctly: if not, it will false report zero dependencies.
-        """
-        p = os.path.join(
-            project,
-            'package',
-            'node_modules',
-            '.package-lock.json')
-        # NPM does not create the node_modules directory for packages with
-        # zero dependencies. However, it also does not create node_modules for
-        # packages that fail to install.
-        if not os.path.isfile(p):
-            return 0
-        with open(p, 'r') as f:
-            data = json.load(f)
-            n = 0
-            for _, v in data['packages'].items():
-                if 'link' in v and v['link']:
-                    continue
-                n += 1
-            return n
 
     def project_result_evaluation(self, dir: str) -> SolveResultEvaluation:
         """
@@ -153,7 +161,7 @@ class Gather(object):
             with open(lock_path, 'r') as f:
                 lock_json = json.load(f)
 
-        eval_result = SolveResultEvaluation(time, status, lock_json)
+        eval_result = SolveResultEvaluation(time, status, lock_json, lock_path)
         return eval_result
 
     def gather(self):
