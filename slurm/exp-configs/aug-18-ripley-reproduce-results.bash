5ef314d0d13e95fd6bcc79c1a53352b28c4b1d0e
Donald Pinckney <donald_pinckney@icloud.com>
2022-08-18 15:26:50 -0400

    Add group size option

--- slurm/exp-configs/aug-18-ripley-reproduce-results.bash
+++ slurm/exp-configs/aug-18-ripley-reproduce-results.bash
@@ -10,5 +10,5 @@ export Z3_LOC=/proj/pinckney/pacsolve/z3/build/z3
 export Z3_MODEL_OPTION=True
 
 mkdir -p $EXPERIMENT_DIR
-./main.py run --cpus-per-task 4 --on-ripley --tarball-dir $TARBALL_DIR --z3-abs-path $Z3_LOC --z3-add-model-option $Z3_MODEL_OPTION --target $EXPERIMENT_DIR --which-experiment $EXPERIMENT_TYPE
+./main.py run --cpus-per-task 4 --max-groups 250 --on-ripley --tarball-dir $TARBALL_DIR --z3-abs-path $Z3_LOC --z3-add-model-option $Z3_MODEL_OPTION --target $EXPERIMENT_DIR --which-experiment $EXPERIMENT_TYPE
 
