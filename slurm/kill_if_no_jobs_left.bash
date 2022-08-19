b8258731667d958716926de6860af4e01ceb60e5
Donald Pinckney <donald_pinckney@icloud.com>
2022-08-18 22:41:20 -0400

    Add kill script

---
+++ slurm/kill_if_no_jobs_left.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sleep 60
+
+while true
+do
+    num_jobs_left=$(squeue -u $USER -h | wc -l)
+    if [ "$num_jobs_left" -eq "0" ]; then
+        echo "No jobs left:"
+        squeue -u $USER
+        sleep 60
+        echo "Killing python3"
+        pkill -9 "python3"
+        exit;
+    fi
+
+    sleep 60
+done
